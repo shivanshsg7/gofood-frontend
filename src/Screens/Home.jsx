fb0d43a3397f62eb9fdf8be063bd25fe8a0977b8
Shivansh Gupta <shivanshsg7@gmail.com>
2025-07-12 00:27:06 +0530

    Initial frontend push

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   const loadData = async () => {
     setLoading(true);
     try {
-      const response = await fetch("https://gofood-backend-oqxu.onrender.com/api/displayData", {
+      const response = await fetch("https://gofood-backend-oqxu.onrender.com/api/foodData", {
 
         method: "POST",
         headers: {
